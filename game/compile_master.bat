017bc578eeaad97c18525bae9607536259dfd970
mhughson <contact@matthughson.com>
2021-10-30 19:52:24 -0700

    * Build standard NES version (not VS) by default. * Create BUILD directory, to avoid compile errors first time building.

--- game/compile_master.bat
+++ game/compile_master.bat
@@ -23,8 +23,8 @@ IF DEFINED audio (
 IF DEFINED code (
 	REM -g adds debug information, but the end result .nes file is not
 	REM affected, so leave it in all the time.
-	cc65 -g -Oirs -D VS_SYS_ENABLED=1 -D VS_SRAM_ENABLED=0 %name%.c --add-source
-	ca65 -D VS_SYS_ENABLED=1 -D VS_SRAM_ENABLED=0 crt0.s
+	cc65 -g -Oirs -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 %name%.c --add-source
+	ca65 -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 crt0.s
 	ca65 %name%.s -g
 
 	REM -dbgfile does not impact the resulting .nes file.
@@ -32,6 +32,7 @@ IF DEFINED code (
 
 	del *.o
 
+	mkdir BUILD\
 	move /Y %name%.dbg BUILD\ 
 	move /Y labels.txt BUILD\ 
 	move /Y %name%.s BUILD\ 
