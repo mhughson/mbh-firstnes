2d9bb2bd18f1fa554f55ca56a7043e78fecf1e0d
mhughson <contact@matthughson.com>
2021-12-17 18:59:58 -0800

    Added GB folder with ZGB template project. Not including any From Below content or code yet.

---
+++ gb/README.md
@@ -0,0 +1,2 @@
+# ZGB-template
+A template for projects using [ZGB](https://github.com/Zal0/ZGB), A little engine for creating games for the original GameBoy
---
+++ gb/clean.bat
@@ -0,0 +1,7 @@
+cd src
+%ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=Release
+%ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=Debug
+%ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=ReleaseColor
+%ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=DebugColor
+cd ..
+pause
